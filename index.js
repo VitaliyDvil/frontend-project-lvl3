2e18b76d49d9d4679eb267c13af33659fd1a8f44
vitaliydvil <domanevskiyvsbel@yandex.ru>
2021-08-09 20:54:50 +0300

    first step

---
+++ index.js
@@ -0,0 +1,10 @@
+import './main.css';
+import 'bootstrap';
+
+console.log('xyu tebe a ne hello');
+
+const a = 1;
+
+function k() {
+    console.log('dssfsf');
+}
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, './index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+          { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+          { test: /\.(js)$/, use: 'babel-loader', exclude: /(node_modules)/ },
+        ],
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+    ],
+}
